88e6b4c94318e366fbf6b5d9744e8eb5de5f6ef3
Anand Eichner <anand@ufo.at>
2020-06-16 03:05:21 +0200

    fix invalid material

--- TankGame/src/Projector.cpp
+++ TankGame/src/Projector.cpp
@@ -7,7 +7,7 @@ void Projector::interact(physx::PxRigidBody * actor, physx::PxRigidBody * invoke
 		auto& lightManager = gameLogic.getScene().getLightManager();
 		lightManager.shadowLightUsed = true;
 		lightManager.UpdateBuffer();
-		gameLogic.getScene().AddObject(std::make_unique<GameObject>(Transform(glm::vec3(2.241f, 1.208f, -0.044f), glm::vec3(1.5708f, 0, 1.52319f), glm::vec3(1.131f, 1.131f, 1.131f)), resourceManager.GetMesh("FilmReel"), resourceManager.GetMaterial("ProjectorMaterial"), "FilmReelOnProjector"));
+		gameLogic.getScene().AddObject(std::make_unique<GameObject>(Transform(glm::vec3(2.241f, 1.208f, -0.044f), glm::vec3(1.5708f, 0, 1.52319f), glm::vec3(1.131f, 1.131f, 1.131f)), resourceManager.GetMesh("FilmReel"), resourceManager.GetMaterial("Projector"), "FilmReelOnProjector"));
 		Inventory::instance().removeFilmreel();
 	}
 }
